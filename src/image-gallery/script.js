156860573992a1fc2afbe53de5a98aebcd694865
James Stuckey Weber <james@oddbird.net>
2025-10-09 14:37:01 -0400

    Add alt text, credits

--- src/image-gallery/script.js
+++ src/image-gallery/script.js
@@ -1,13 +1,17 @@
 const dialogElem = document.getElementById("dialog");
 const dialogImageElem = document.getElementById("dialog-image");
+const dialogCreditElem = document.getElementById("dialog-credit");
 const showBtns = document.querySelectorAll(".show");
 const closeBtn = document.querySelector(".close");
 
 [...showBtns].forEach((showBtn) => {
   showBtn.addEventListener("click", (event) => {
-    // Assumes the image is the previous sibling
-    const imgSrc =
-      event.currentTarget.previousElementSibling.getAttribute("src");
+    const listItem = event.currentTarget.closest("li");
+
+    const creditHTML = listItem.querySelector(".credit").innerHTML;
+    dialogCreditElem.innerHTML = creditHTML;
+    
+    const imgSrc = listItem.querySelector("img").getAttribute("src");
     dialogImageElem.setAttribute("src", imgSrc);
     dialogImageElem.addEventListener("load", () => {
       dialogElem.showModal();
